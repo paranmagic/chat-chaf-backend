aa0bc534f6ec0495ac074492cc2833d21ddf334a
paranmagic <junman21@gmail.com>
2024-11-27 17:43:15 +0900

    api등록완료

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+import * as dotenv from "dotenv";
+import path from "path";
+
+// env 설정
+const __dirname = path.resolve();
+dotenv.config({ path: __dirname + "/.env" });
+
 console.log('hello world');
+console.log(process.env.OPENAI_API_KEY);
