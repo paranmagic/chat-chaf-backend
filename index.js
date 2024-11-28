ff22dd9194ed7a8f6cb7f7ed6be8f176dacad2f0
paranmagic <junman21@gmail.com>
2024-11-28 10:54:57 +0900

    GET 호출

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+import express from "express";
+import cors from "cors";
 import * as dotenv from "dotenv";
 import path from "path";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extende: true}));
 
 // env 설정
 const __dirname = path.resolve();
 dotenv.config({ path: __dirname + "/.env" });
 
-console.log('hello world');
-console.log(process.env.OPENAI_API_KEY);
+// test 코드
+app.get('/test', async (req, res) => {
+  try{
+    res.json({data:'AI 서비스 개발 강의'});
+
+  }catch(error){
+
+  }
+});
+
+// get-읽기, post-추가, update-수정, delete-삭제
+app.listen('8080');
